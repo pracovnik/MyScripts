2823e18b8885586f272fbfd8559f46364986b707
pracovnik <40662523+pracovnik@users.noreply.github.com>
2022-10-19 10:18:33 +0200

    Create mjpg_streamer_docker_command.sh

---
+++ docker/mjpg_streamer_docker_command.sh
@@ -0,0 +1,14 @@
+docker run \
+    --detach \
+    --name webcam1 \
+    --device "/dev/v4l/by-id/usb-046d_0825_9F83CFA0-video-index0:/dev/video0" \
+    --entrypoint mjpg_streamer \
+    -p 18080:8080 \
+    kvaps/mjpg-streamer \
+    -i "/usr/lib64/input_uvc.so -y -d /dev/video0 -r 1280x960 -f 30 " \
+    -o "/usr/lib64/output_http.so -p 8080 -w /usr/share/mjpeg-streamer/www/ -c admin:admin"
+
+
+# get uniqu ID of the device as /dev/videoX changes
+# ls -al /sys/class/video4linux
+
