5bfe2cea39afb865e88cfbac22e8c1f5ee4c9cf7
pracovnik <40662523+pracovnik@users.noreply.github.com>
2022-10-31 09:18:01 +0100

    Update webcam2_mjpg-streamer_docker.sh

--- docker/webcam2_mjpg-streamer_docker.sh
+++ docker/webcam2_mjpg-streamer_docker.sh
@@ -10,7 +10,7 @@ docker run \
     -p 18082:8080 \
     kvaps/mjpg-streamer \
     -i "/usr/lib64/input_uvc.so -y -d /dev/video0 -r 1280x960 -f 30 " \
-    -o "/usr/lib64/output_http.so -p 8080 -w /usr/share/mjpeg-streamer/www/ -c ivo:Win"
+    -o "/usr/lib64/output_http.so -p 8080 -w /usr/share/mjpeg-streamer/www/ -c admin:admin"
 
 
 # get uniqu ID of the device as /dev/videoX changes
